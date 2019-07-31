ec491b23109beb746425520a81a9fdf1edc26ed5
Oscar Bataille <oscar.bataille@vessinge.se>
2019-07-31 16:22:15 +0200

    clean vendor

--- src/config.php
+++ src/config.php
@@ -13,7 +13,6 @@ return [
             'apiEndpoint' => 'api/bostad',
         ],
     ],
-    'domain'    => 'https://u4pp.u4a.se/FN667500P/',
     'twilio'    => [
         'sid'   => 'AC69df257d00521bee4c1439a7854ea4fe',
         'token' => '61e22785fe2845664d75b619f3ce66d3',
